fbd13b3e360e6e33a617d9d42540ee56ec5a1053
Frédéric Menou <frederic.menou@gmail.com>
2010-08-08 02:09:31 +0200

    Génération du projet sous eclipse

---
+++ cartographs/android/src/org/kercoin/tekku/carto/Cartograph.java
@@ -0,0 +1,13 @@
+package org.kercoin.tekku.carto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Cartograph extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
