41ef46b3f051afe4024c95df9518005f1d4e9421
Frédéric Menou <frederic.menou@gmail.com>
2010-08-15 18:35:46 +0200

    Ecran de configuration 1 sur 2.

--- cartographs/android/gen/org/kercoin/tekku/carto/R.java
+++ cartographs/android/gen/org/kercoin/tekku/carto/R.java
@@ -8,16 +8,35 @@
 package org.kercoin.tekku.carto;
 
 public final class R {
+    public static final class array {
+        public static final int lines=0x7f040001;
+        public static final int networks=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int btn_1of2_next=0x7f060005;
+        public static final int btn_1of2_previous=0x7f060004;
+        public static final int btn_2of2_next=0x7f060001;
+        public static final int btn_2of2_previous=0x7f060000;
+        public static final int line=0x7f060003;
+        public static final int network=0x7f060002;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int 2of2=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050002;
+        public static final int btn_next=0x7f050007;
+        public static final int btn_previous=0x7f050006;
+        public static final int carto_1of2=0x7f050005;
+        public static final int description=0x7f050000;
+        public static final int hello=0x7f050001;
+        public static final int menu_option=0x7f050003;
+        public static final int title=0x7f050004;
     }
 }
--- cartographs/android/src/org/kercoin/tekku/carto/Cartograph.java
+++ cartographs/android/src/org/kercoin/tekku/carto/Cartograph.java
@@ -2,6 +2,10 @@ package org.kercoin.tekku.carto;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Cartograph extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,21 @@ public class Cartograph extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Spinner network = (Spinner) findViewById(R.id.network);
+        ArrayAdapter<CharSequence> networkAdapter = ArrayAdapter.createFromResource(
+        	    this, R.array.networks, android.R.layout.simple_spinner_item);
+		network.setAdapter(networkAdapter);
+		Spinner line = (Spinner) findViewById(R.id.line);
+		ArrayAdapter<CharSequence> lineAdapter = ArrayAdapter.createFromResource(
+				this, R.array.lines, android.R.layout.simple_spinner_item);
+		line.setAdapter(lineAdapter);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	boolean superResult = super.onCreateOptionsMenu(menu);
+    	MenuItem option = menu.add(R.string.menu_option);
+    	option.setIcon(R.drawable.icon);
+		return superResult;
     }
 }
